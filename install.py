75f8363f90905b4b7c43ab2f9803855fcd6040ad
Rwwwrl <forgithublesh@yandex.ru>
2022-01-18 17:55:43 +0300

    [add]
    - добавил класс FileTook и удаление и запуск makemigrations и migrate в скрипт инсталл.

--- install.py
+++ install.py
@@ -1,8 +1,9 @@
-from services.db_tool import DbTool
+import os
 
-import os, django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
-# django.setup()
+from services.db_tool import DbTool
+from services.file_tool import FileTool
+from services import print_service as p
+
 
 class Installer:
 
@@ -12,8 +13,17 @@ class Installer:
             postgres_db_instance.drop_project_databases()
             postgres_db_instance.create_project_databases()
 
+    @staticmethod
+    def delete_and_update_migrations():
+        f = FileTool(['app']) 
+        f.delete_migration_files()
+        f.makemigrations_and_migrate() 
+
 
 if __name__ == '__main__':
+    p.info('НАЧАЛО СКРИПТА')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
     Installer.drop_and_create_dbs()
-    
+    Installer.delete_and_update_migrations() 
+    p.success('СКРИПТ УСПЕШНО ВЫПОЛНЕН')
 
---
+++ services/file_tool.py
@@ -0,0 +1,43 @@
+import os
+from sys import platform
+
+from services import print_service as p
+
+
+
+class FileTool:
+
+
+    def __new__(cls, apps_list):
+        cls.apps_list = apps_list
+        cls.python_command = 'sudo python3' if platform == 'linux' else 'python'
+        return super().__new__(cls)
+
+    @classmethod
+    def delete_migration_files(cls):
+        '''
+        удалить файлы миграций из папок приложений 
+        '''
+        root_project_path = os.getcwd()
+        for app in cls.apps_list:
+            migration_folder_path = os.path.join(root_project_path, app, 'migrations')
+            for file in os.listdir(migration_folder_path):
+                if not file == '__init__.py':
+                    file_path = os.path.join(migration_folder_path, file)
+                    if os.path.isfile(file_path):
+                        os.remove(file_path)
+        p.info('Были удалены файлы миграции')
+    
+    @classmethod
+    def makemigrations_and_migrate(cls):
+        cls.__run_python_command('makemigrations')
+        cls.__run_python_command('migrate')
+
+    @classmethod
+    def __run_python_command(cls, python_command):
+        command = cls.python_command + ' manage.py ' + python_command
+        result = os.system(cls.python_command + ' manage.py ' + python_command)
+        if result:
+            p.info(command + ' успешно проведена')
+        else:
+            p.error(command + ' проведена с ошибкой')
